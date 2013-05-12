93c6f6c5d30817c126e229a2ded626c3393039b2
Jukka Svahn <web@rahinaa.biz>
2013-05-12 13:45:21 +0300

    Updated the header.

--- rah_backup_clearlogs.php
+++ rah_backup_clearlogs.php
@@ -1,15 +1,19 @@
 <?php
 
 /**
- * Empties visitor logs before taking a backup with rah_backup.
+ * Clear logs module for rah_backup.
  *
- * @package rah_backup
- * @author Jukka Svahn <http://rahforum.biz>
- * @copyright (c) 2011 Jukka Svahn
- * @license GLPv2
- * 
- * Note that the module will empty the actual table, not just records from backup files.
- * Visitor logs will be permanently lost.
+ * Empties visitor logs before taking a backup with rah_backup.
+ * Note that the module will empty the actual table, not just
+ * records from backup files. Visitor logs will be permanently lost.
+ *
+ * @author  Jukka Svahn
+ * @license GNU GPLv2
+ * @link    https://github.com/gocom/rah_backup_clearlogs
+ *
+ * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
+ * Licensed under GNU General Public License version 2
+ * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 	register_callback('rah_backup__clearlogs', 'rah_backup.create');
